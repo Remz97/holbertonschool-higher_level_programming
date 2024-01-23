4af7fd545108ae507e3d8be9d67c63dfe9945be7
Remz97 <remzikeqi@icloud.com>
2024-01-23 15:14:57 +0100

    ex6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num in range(0, 10):
     for num_2 in range(num+1, 10):
         if num == 8 and num_2 == 9:
-            print(f"{num}{num_2}")
+            print("{}{}".format(num, num_2))
         else:
-            print(f"{num}{num_2}, ", end="")
+            print("{}{}, ".format(num, num_2), end="")
