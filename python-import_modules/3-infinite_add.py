587a0b51a07dddf544bb77ebff584a6d993da348
Remz97 <remzikeqi@icloud.com>
2024-01-25 10:55:47 +0100

    ex3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    num_arg = len(argv) - 1
+    if num_arg == 0:
+        print("0")
+    else:
+        sum = 0
+        for i in range(1, num_arg + 1):
+            sum = sum + int(argv[i])
+    print("{}".format((sum)))
