bf895a821177bff9d73eada1d81cf87983a302e4
Remz97 <remzikeqi@icloud.com>
2024-01-28 12:44:58 +0100

    ex9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-        for num in my_list:
-            if len(my_list) == 0:
-              return None
-            else:
-                my_list.sort()
-        print("Max: {:d}".format(my_list[-1]))
-
-
-my_list = [0, 9, 7]
-max_value = max_integer(my_list)
+    if len(my_list) == 0:
+        return None
+    else:
+        my_list.sort()
+        return my_list[-1]
