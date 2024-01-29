641942193d508e07bae0aaf1ae798da17259d3d0
Remz97 <remzikeqi@icloud.com>
2024-01-29 13:22:54 +0100

    ex1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for el in range(len(new_list)):
+        if new_list[el] == search:
+            new_list[el] = replace
+    return new_list
+
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
