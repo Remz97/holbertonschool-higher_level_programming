c53e09c750eaa6895a8bcc255db5a8ba40a2ab5d
Remz97 <remzikeqi@icloud.com>
2024-01-30 11:05:56 +0100

    update

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@ def roman_to_int(roman_string):
     if isinstance(roman_string, str) is False or roman_string is None:
         return 0
     rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    conv = 0
+    result = 0
     for i in range(len(roman_string) - 1):
         if rom_num[roman_string[i]] < rom_num[roman_string[i + 1]]:
-            conv -= rom_num[roman_string[i]]
+            result -= rom_num[roman_string[i]]
         else:
-            conv += rom_num[roman_string[i]]
-    conv += rom_num[roman_string[-1]]
-    return conv
+            result += rom_num[roman_string[i]]
+    result += rom_num[roman_string[-1]]
+    return result
