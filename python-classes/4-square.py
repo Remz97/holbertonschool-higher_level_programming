79b59e1a3bf668b3295567fef5e85f640c23a4ba
Remz97 <remzikeqi@icloud.com>
2024-01-31 17:22:15 +0100

    ex4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,20 +10,23 @@ class Square():
     '''
     def __init__(self, size=0):
         self.__size = size
-    
+
     @property
     def size(self):
+        """Get size of square"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         self.__size = value
-
+        '''Initializes the data'''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
+        '''returns the current square'''
         return self.__size * self.__size
