222983f441b9247b9a371e30c53afdbb66ab7910
Remz97 <remzikeqi@icloud.com>
2024-01-31 14:47:09 +0100

    ex1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,6 +2,11 @@
 '''
 add a private instance attribute: size
 '''
+
+
 class Square():
+    '''
+    adding a private instance attribute: size
+    '''
     def __init__(self, size):
         self.__size = size
