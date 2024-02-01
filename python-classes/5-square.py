0cba6d659eced8b53481be4c76891501ed54ff28
Remz97 <remzikeqi@icloud.com>
2024-02-01 11:47:04 +0100

    ex5

---
+++ python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+add a private instance attribute: size
+'''
+
+
+class Square():
+    '''
+    adding a private instance attribute: size
+    '''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        '''Initializes the data'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        '''returns the current square'''
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
