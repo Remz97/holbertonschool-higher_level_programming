e7d74f8d17b74e7d7ad22155010e0f928265b2dd
Remz97 <remzikeqi@icloud.com>
2024-02-02 16:19:17 +0100

    ex6

---
+++ python-classes/6-square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+'''
+add a private instance attribute: size
+'''
+
+
+class Square():
+    '''
+    adding a private instance attribute: size
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Get size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        '''Initializes the data'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Initializes the data."""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not all(isinstance(i, int) and i >= 0 for i in value):
+            raise ValueError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        '''returns the current square'''
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+    
+   
+    
+
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
