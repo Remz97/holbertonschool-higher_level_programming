4fb44e0b83f731f6f85c47b67dc4e4aa9b2c61ab
Remz97 <remzikeqi@icloud.com>
2024-02-06 10:17:06 +0100

    ex4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -60,4 +60,29 @@ class Rectangle:
             return result
 
     def __repr__(self):
-        return f"Rectangle({self.height}, {self.width})"
+        return f"Rectangle({self.width}, {self.height})"
+
+
+my_rectangle = Rectangle(2, 4)
+print(str(my_rectangle))
+print("--")
+print(my_rectangle)
+print("--")
+print(repr(my_rectangle))
+print("--")
+print(hex(id(my_rectangle)))
+print("--")
+
+# create new instance based on representation
+new_rectangle = eval(repr(my_rectangle))
+print(str(new_rectangle))
+print("--")
+print(new_rectangle)
+print("--")
+print(repr(new_rectangle))
+print("--")
+print(hex(id(new_rectangle)))
+print("--")
+
+print(new_rectangle is my_rectangle)
+print(type(new_rectangle) is type(my_rectangle))
