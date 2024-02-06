d56f28c526bec5d46a8d18c33691c42f76e3447a
Remz97 <remzikeqi@icloud.com>
2024-02-06 13:47:38 +0100

    ex8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
-   
+
     @property
     def height(self):
         return self.__height
@@ -81,19 +81,3 @@ class Rectangle:
             return rect_1
         else:
             rect_1
-
-my_rectangle_1 = Rectangle(8, 4)
-my_rectangle_2 = Rectangle(2, 3)
-
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
-
-
-my_rectangle_2.width = 10
-my_rectangle_2.height = 5
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
