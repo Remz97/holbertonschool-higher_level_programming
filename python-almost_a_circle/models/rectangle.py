3cb654949177e9d0ffff442f307f49a173ec1eeb
Remz97 <remzikeqi@icloud.com>
2024-02-13 14:45:53 +0100

    rectan

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,5 +73,5 @@ class Rectangle(Base):
             print("#" * self.width)
 
     def __str__(self):
-        return (f"[Rectangle] ({self.id}) {self.x} / {self.y} - "
-                f"{self.width} / {self.height}")
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
+                f"{self.width}/{self.height}")
