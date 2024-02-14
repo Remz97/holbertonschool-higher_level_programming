20c648669bdbe914703c05bb2ae05255508b22e5
Remz97 <remzikeqi@icloud.com>
2024-02-14 10:46:28 +0100

    dict

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,10 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if key in attributes:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {'id': self.id,
+                 'width': self.width,
+                'height': self.height,
+                 'x': self.x,
+                 'y': self.y}
