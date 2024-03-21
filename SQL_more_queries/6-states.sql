0604e7633e6fe56f9030024c5c30b28cfab2036f
Remz97 <remzikeqi@icloud.com>
2024-03-21 10:33:08 +0100

    ex6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- create database and table
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL
 );
