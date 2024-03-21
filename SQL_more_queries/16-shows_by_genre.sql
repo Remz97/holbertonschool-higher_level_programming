efb7975c18d2a38711d65a9527b511a5da1e1519
Remz97 <remzikeqi@icloud.com>
2024-03-21 11:34:47 +0100

    ex16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
--- Comedy shows
-SELECT tv_shows.title
+-- shows and genres
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
